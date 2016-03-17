b6e5816c944b9265348814e510acd62145906787
Idris Raja <idris.raja@gmail.com>
2016-03-17 18:41:41 -0500

    record game state

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -8,6 +8,13 @@
 
 #import "ViewController.h"
 
+typedef NS_ENUM(NSInteger, GameState) {
+    GameStateEmtpy = 1,
+    GameStateTie,
+    GameStateXWin,
+    GameStateOWin
+};
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *button1;
 @property (weak, nonatomic) IBOutlet UIButton *button2;
@@ -18,21 +25,46 @@
 @property (weak, nonatomic) IBOutlet UIButton *button7;
 @property (weak, nonatomic) IBOutlet UIButton *button8;
 @property (weak, nonatomic) IBOutlet UIButton *button9;
-@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *turnLabel;
 @property NSUInteger turnIndex;
-@property NSString *boardState;       // 0 for "O", 1 for "X", undefined if not yet played
+@property NSMutableString *boardState;       // 0 for "O", 1 for "X", undefined if not yet played
 @property NSString *playerTurn;
+@property NSArray* winningGameStates;
 
 @end
 
 @implementation ViewController
 
++ (NSArray *)winningGameStates {
+    static NSArray *_winningGameStates;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+                            //  "123456789"
+        _winningGameStates = @[@"XXX      ",
+                               @"   XXX   ",
+                               @"      XXX",
+                               @"X  X  X  ",
+                               @" X  X  X ",
+                               @"  X  X  X",
+                               @"X   X   X",
+                               @"  X X X  "];
+    });
+    return _winningGameStates;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.turnIndex = 0;
-    self.boardState = [@"" stringByPaddingToLength:9 withString: @" " startingAtIndex:0];
-    self.whichPlayerLabel.font = [UIFont systemFontOfSize:25];
+    NSString *startingBoardState = [@"" stringByPaddingToLength:9 withString: @" " startingAtIndex:0];
+    self.boardState = [NSMutableString stringWithString:startingBoardState];
+    self.turnLabel.font = [UIFont systemFontOfSize:25];
     self.playerTurn = @"X";
+    [self updateTurnLabel];
+}
+
+-(void) updateTurnLabel {
+    self.turnLabel.text = self.playerTurn;
 }
 
 -(void) togglePlayerTurn {
@@ -41,34 +73,38 @@
     } else {
         self.playerTurn = @"X";
     }
+    [self updateTurnLabel];
 }
 
 -(IBAction) onButtonTapped:(UIButton*)button {
-    NSInteger tag = button.tag;
-    NSInteger index = tag / 10 - 1;
-    NSString *currentValue = [button.titleLabel.text substringWithRange:NSMakeRange(index, 1)];
+    NSUInteger tag = button.tag;
+    NSUInteger index = tag / 10 - 1;
+    NSString *currentValue = button.titleLabel.text;
 
-    // check if button is blank to proceed
-    if (!currentValue) {
+    if (currentValue == nil) {
+        // update button appearance
         [button setTitle:self.playerTurn forState:UIControlStateNormal];
+        if ([self.playerTurn isEqualToString:@"X"]) {
+            [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        } else {
+            [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        }
+
+        // update game board state
+        NSString *updatedBoardState =[self.boardState stringByReplacingCharactersInRange:NSMakeRange(index, 1) withString:self.playerTurn];
+        self.boardState = [NSMutableString stringWithString:updatedBoardState];
         [self togglePlayerTurn];
+        NSLog(self.boardState);
+
+
     } else {
         // TODO: give user feedback
     }
 }
 
-- (IBAction)winner:(UIButton *)sender {
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Winner!"
-                                                                             message:@"Someone Won!"
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
+-(void) analyzeGameState {
+    NSArray *winningGameStates = [self winningGameStates];
 
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"cancel"
-                                                           style:UIAlertActionStyleCancel
-                                                         handler:nil];
-
-    [alertController addAction:cancelAction];
-    [self presentViewController:alertController animated:YES completion:nil];
 }
-
 
 @end
