57c96c34af7e9616f2f4b28cccf738d36afc761b
Idris Raja <idris.raja@gmail.com>
2016-03-17 15:32:49 -0500

    toggle turn indicator

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,15 +9,18 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *one;
-@property (weak, nonatomic) IBOutlet UIButton *two;
-@property (weak, nonatomic) IBOutlet UIButton *three;
-@property (weak, nonatomic) IBOutlet UIButton *four;
-@property (weak, nonatomic) IBOutlet UIButton *five;
-@property (weak, nonatomic) IBOutlet UIButton *six;
-@property (weak, nonatomic) IBOutlet UIButton *seven;
-@property (weak, nonatomic) IBOutlet UIButton *eight;
-@property (weak, nonatomic) IBOutlet UIButton *nine;
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property NSUInteger turnIndex;
 
 @end
 
@@ -25,10 +28,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.turnIndex = 0;
+    self.whichPlayerLabel.font = [UIFont systemFontOfSize:25];
+
+    
 }
--(IBAction) onButtonTapped:(UIButton*)button
 
+-(IBAction) onButtonTapped:(UIButton*)button {
+    self.turnIndex++;
 
+    if (self.turnIndex % 2 == 0) {
+        self.whichPlayerLabel.text = @"O";
+        self.whichPlayerLabel.textColor = [UIColor redColor];
+    } else {
+        self.whichPlayerLabel.text = @"X";
+        self.whichPlayerLabel.textColor = [UIColor blueColor];
+    }
+}
+
+-(void) toggleTurnIndicator {
+    //    NSString *currentTurnLabel = self.whichPlayerLabel.text;
+}
+
+//-(void) toggleButtonText:(UIButton*)button {
+//    if ([button.titleLabel.text isEqualToString:@"X"]) {
+//        // make label O
+//        [button setTitle:@"O" forState:UIControlStateNormal];
+//        button.textColor = [UIColor redColor];
+//    } else {
+//        // make label X
+//        [button setTitle:@"X" forState:UIControlStateNormal];
+//        button.textColor = [UIColor blueColor];
+//    }
+//}
 
 @end
