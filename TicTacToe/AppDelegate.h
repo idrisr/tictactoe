d31bad7628e74dfcd2ac3d262b5ddeafb97b2f08
Brandon Gress <WorkCompXD@brandons-macbook-pro.local>
2016-03-17 14:20:02 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Brandon Gress on 3/17/16.
+//  Copyright © 2016 Brandon Gress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Brandon Gress on 3/17/16.
+//  Copyright © 2016 Brandon Gress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
